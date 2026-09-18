2226d78e21cda96e09809311f8d11e688fb39009
Omaima Assif <omaima.assif-etu@etu.univh2c.ma>
2026-09-18 14:23:00 +0100

    Create subtree-of-a-binary-tree.java

---
+++ subtree-of-a-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        boolean result = false;
+        if(isSameTree(root, subRoot)){
+            result = true;
+        }
+        else {
+            if(root.left != null){
+                return isSubtree(root.left, subRoot); 
+            } if (root.right != null){
+                return isSubtree(root.right, subRoot); 
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        
+        if((p==null && q!=null )|| (q==null && p!=null) || (p.val != q.val)) {
+            return false; 
+        }
+
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    }
+}
