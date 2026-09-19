2477faee773a407fcf550f43ad249ffa4ef4bffb
Omaima Assif <omaima.assif-etu@etu.univh2c.ma>
2026-09-19 17:19:46 +0100

    Create binary-tree-right-side-view.java

---
+++ binary-tree-right-side-view.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null){
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+
+            int lastLevelElement = 0;
+            int levelSize = queue.size();
+
+            for(int i=0; i<levelSize; i++){
+                TreeNode node = queue.poll();
+                lastLevelElement = node.val;
+
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(lastLevelElement);
+        }
+        return result;
+    }
+}
